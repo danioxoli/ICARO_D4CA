2db8eab55d34990d5e372a3a72ecbb10c933988f
DOxoli <danioxoli@users.noreply.github.com>
2017-09-20 12:39:19 +0200

    Update planet_dos.py

--- sat_imagery/planet_dos.py
+++ sat_imagery/planet_dos.py
@@ -3,9 +3,11 @@
 Created on Wed Aug 23 17:00:26 2017
 #
 # Project:  ICARO - D4CA
-# Purpose:  MERGE multiple tiles and perform ToA and DOS correction on PlanetScope Imagery (https://www.planet.com/docs/spec-sheets/sat-imagery)
+# Purpose:  MERGE multiple tiles and perform ToA and DOS correction on PlanetScope Imagery 
+            (https://www.planet.com/docs/spec-sheets/sat-imagery)
 # Author:   Daniele Oxoli, Monia Elisa Molinari (daniele.oxoli, moniaelisa.molinari@polimi.it)
-# Affiliation: Dep. of Civil and Environmental Engineering, Politecnico di Milano, P.zza Leonardo da Vinci 32, 20133, Milano, Italy
+# Affiliation: Dep. of Civil and Environmental Engineering, Politecnico di Milano, 
+               P.zza Leonardo da Vinci 32, 20133, Milano, Italy
 #
 """
 ###############################################################################
